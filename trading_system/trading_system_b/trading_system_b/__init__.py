8af104c552fa7930273159dbc76a748354d5b4a7
wAGswKX <szcaoxinyue@163.com>
2026-05-10 22:06:52 +0800

    Delete trading_system/trading_system_b directory

--- trading_system/trading_system_b/trading_system_b/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Backend modules for the B-part trading system."""
-
-from .config import AppConfig
-from .db import Database
-from .execution import SimulatedExecutionEngine
-
-__all__ = ["AppConfig", "Database", "SimulatedExecutionEngine"]
